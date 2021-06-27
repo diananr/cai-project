907df5844d48fda8436452a32a70f7a2f7657ff4
Diana Navarro <navarroruiz.diana@gmail.com>
2021-06-27 17:18:23 -0600

    feat: include Row struct

--- back/api.go
+++ back/api.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Row struct {
+	CAI   		float64  `json:"cai"`
+	Age   		float64	 `json:"age"`
+	Work   		float64  `json:"work"`
+	Link   		float64  `json:"link"`
+	Alcohol   float64  `json:"alcohol"`
+	Smoke   	float64  `json:"smoke"`
+	Drugs   	float64  `json:"drugs"`
+	Addiction float64  `json:"addiction"`
+	Risk   		float64  `json:"risk"`
+	Month  	 	float64  `json:"month"`
+}
 
 
 var mainDataset [][]float64
@@ -17,11 +31,17 @@ func makePrediction(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, "Error reading request body",
 				http.StatusInternalServerError)
 		}
-		bodyJson := string(body)
-		fmt.Println("bodyJson =>", bodyJson)
+
+		var rowBody Row
+		err = json.Unmarshal(body, &rowBody)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
 
 		rowToTest :=  mainDataset[3]
 		predictionResponse := predictClassification(mainDataset, rowToTest, 5)
+		fmt.Println("predictionResponse =>", predictionResponse)
 
 	} else {
 		http.Error(res, "Invalid request method", http.StatusMethodNotAllowed)
